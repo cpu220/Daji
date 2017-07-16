3270547820723dbaa13393e03df92522ebe4ace7
Belial <102864814@qq.com>
2017-07-16 18:02:17 +0800

    [update] ip

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,8 @@ const program = require('commander'),
  appInfo = require('./../package.json'),
  project = require('../lib/project.js'),
  ios = require('../lib/ios.js'),
+ ip = require('../lib/ip.js'),
+
  color = require('colors-cli/toxic');
 
 
@@ -10,6 +12,7 @@ program
     // .allowUnknownOption()//不报错误
     .version(appInfo.version)
     .usage('妲己是个小能手,什么都可以做,目前还正在学习ing'.x31+' [options] <package>')
+    .description('目前正在测试ing，而且此工具基本不对外，呵呵'.x33)
     .parse(process.argv);
 
 program
@@ -22,13 +25,13 @@ program
       const a = typeof options.name === 'string'?options.name:''
       project(cmd,options);
     }).on('--help', function() {
- 			console.log('welcome Daji');
+ 			console.log('welcome for Daji');
 
     });
 
 program
   .command('ios [cmd]')
-  .alias('i')
+  .alias('ios')
   .description('this is my ios simulator'.x29)
   .option('-i --init [type]','测试一下')
   .action(function(cmd,options){
@@ -37,11 +40,22 @@ program
     console.log('ios simulator'.x29);
   })
 
+program
+  .command('ip [cmd]')
+  .alias('ip')
+  .description('get this mac\'s ip'.x29)
+  .option('-p --ip [type]','获取ip')
+  .action(function(cmd,options){
+    ip(cmd,options); 
+  }).on('--help',function(){
+    console.log('获取ip');
+  })
 
 //默认不传参数输出help
 if (!process.argv[2]) {
+    // console.log('test 木有参数');
     program.help();
-    console.log('test 木有参数');
+
 }
 
 program.parse(process.argv);
